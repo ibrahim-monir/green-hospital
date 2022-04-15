3255bdf60a249223dddac621776a210d29eabe95
monzuralam <monzur707@gmail.com>
2022-04-16 01:15:18 +0600

    overview section init

---
+++ admin/overview.php
@@ -0,0 +1,28 @@
+<!-- Page header section  -->
+<div class="block-header">
+    <div class="row clearfix">
+        <div class="col-lg-4 col-md-12 col-sm-12">
+            <h1>Hi, Welcomeback!</h1>
+            <span>JustDo Doctors,</span>
+        </div>
+        <div class="col-lg-8 col-md-12 col-sm-12 text-lg-right">
+            <div class="d-flex align-items-center justify-content-lg-end mt-4 mt-lg-0 flex-wrap vivify pullUp delay-550">
+                <div class="border-right pr-4 mr-4 mb-2 mb-xl-0 hidden-xs">
+                    <p class="text-muted mb-1">Permanent</p>
+                    <h5 class="mb-0">214</h5>
+                </div>
+                <div class="border-right pr-4 mr-4 mb-2 mb-xl-0">
+                    <p class="text-muted mb-1">Contract</p>
+                    <h5 class="mb-0">206</h5>
+                </div>
+                <div class="border-right pr-4 mr-4 mb-2 mb-xl-0">
+                    <p class="text-muted mb-1">Total</p>
+                    <h5 class="mb-0">420</h5>
+                </div>
+                <div class="mb-3 mb-xl-0">
+                    <a href="#" class="btn btn-dark">Add New</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
