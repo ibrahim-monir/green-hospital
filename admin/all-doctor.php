0e9bb78af58b413d57721a695ee8cf0ae65bd781
monzuralam <monzur707@gmail.com>
2022-04-18 02:52:10 +0600

    all doctor show dynamicly

--- admin/all-doctor.php
+++ admin/all-doctor.php
@@ -1,6 +1,5 @@
 <?php
 require_once 'header.php';
-require_once 'sidebar.php';
 require_once 'overview.php';
 ?>
 <div class="row clearfix">
@@ -30,27 +29,43 @@ require_once 'overview.php';
                         </tr>
                     </thead>
                     <tbody>
-                        
-                        <tr>
-                            <td class="w60">
-                                <img src="assets/images/xs/avatar4.jpg" data-toggle="tooltip" data-placement="top" title="" alt="Avatar" class="avatar rounded-circle" data-original-title="Avatar Name">
-                            </td>
-                            <td>
-                                <div>Jason Porter</div>
-                                <p class="mb-0">+ 264-625-5858</p>
-                            </td>
-                            <td>jason-porter@example.com</td>
-                            <td>
-                                <div>13 June 2019</div>
-                                <span class="text-muted font-12">7:12PM to 8:30PM</span>
-                            </td>
-                            <td>Dr. David</td>
-                            <td>Fever</td>
-                            <td>
-                                <button type="button" class="btn btn-default btn-sm" title="Edit"><i class="fa fa-pencil"></i></button>
-                                <button type="button" class="btn btn-default btn-sm" title="Delete"><i class="fa fa-trash-o"></i></button>
-                            </td>
-                        </tr>
+                        <?php
+                            $sql = "SELECT * FROM doctors";
+                            $query = mysqli_query( $conn, $sql);
+                            if($query){
+                                while($row = mysqli_fetch_assoc($query)){
+                                    ?>
+                                    <tr>
+                                        <td class="w60">
+                                            <img src="assets/images/xs/avatar4.jpg" data-toggle="tooltip" data-placement="top" title="" alt="Avatar" class="avatar rounded-circle" data-original-title="Avatar Name">
+                                        </td>
+                                        <td>
+                                            <div><?php echo $row['full_name']; ?></div>
+                                            <p class="mb-0"><?php echo $row['designation']; ?></p>
+                                        </td>
+                                        <td><?php echo $row['email']; ?></td>
+                                        <td>
+                                            <?php echo $row['mobile_number']; ?>
+                                        </td>
+                                        <td>
+                                            <?php
+                                                $departmentsql = "SELECT name from departments WHERE id='$row[department_id]'";
+                                                $department = mysqli_query($conn, $departmentsql);
+                                                if($department){
+                                                    echo mysqli_fetch_assoc($department)['name'];
+                                                }
+                                            ?>
+                                        </td>
+                                        <td><?php echo $row['specialist'] ?></td>
+                                        <td>
+                                            <a href="/admin/edit-doctor.php?id=<?php echo $row['id']; ?>" class="btn btn-default btn-sm" title="Edit"><i class="fa fa-pencil"></i></a>
+                                            <a href="/admin/delete-doctor.php?id=<?php echo $row['id']; ?>" class="btn btn-default btn-sm" title="Delete"><i class="fa fa-trash-o"></i></a>
+                                        </td>
+                                    </tr>
+                                    <?php 
+                                }
+                            }
+                        ?>
                     </tbody>
                 </table>
             </div>
