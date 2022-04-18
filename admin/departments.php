7a01218d6abf111e0544193e0a1422ea2b331dc9
monzuralam <monzur707@gmail.com>
2022-04-18 22:16:33 +0600

    departments show dynamicly

---
+++ admin/departments.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'header.php';
+require_once 'overview.php';
+?>
+<div class="row clearfix">
+    <div class="col-12">
+        <div class="card">
+            <div class="body">
+                <form>
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search Departments">
+                        <div class="input-group-append">
+                            <span class="input-group-text" id="search-mail"><i class="icon-magnifier"></i></span>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="table-responsive">
+                <table class="table table-hover mb-0">
+                    <thead>
+                        <tr> 
+                            <th>#</th>
+                            <th>Department Name</th>
+                            <th>Description</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $sql = "SELECT * FROM departments";
+                            $query = mysqli_query( $conn, $sql);
+                            if($query){
+                                while($row = mysqli_fetch_assoc($query)){
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $row['id']; ?></td>
+                                        <td><?php echo $row['name']; ?></td>
+                                        <td><?php echo $row['description']; ?></td>
+                                        <td><?php echo $row['status']; ?></td>
+                                        <td>
+                                            <a href="/admin/edit-department.php?id=<?php echo $row['id']; ?>" class="btn btn-default btn-sm" title="Edit"><i class="fa fa-pencil"></i></a>
+                                            <a href="/admin/delete-department.php?id=<?php echo $row['id']; ?>" class="btn btn-default btn-sm" title="Delete"><i class="fa fa-trash-o"></i></a>
+                                        </td>
+                                    </tr>
+                                    <?php 
+                                }
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+require_once 'footer.php';
