8354d631a0ab3de753f4ef0644fd5e5c39e6340b
monzuralam <monzur707@gmail.com>
2022-06-09 20:31:12 +0600

    update edit department link

--- admin/departments.php
+++ admin/departments.php
@@ -4,6 +4,7 @@ require_once 'overview.php';
 ?>
 <div class="row clearfix">
     <div class="col-12">
+        <!-- Departments start here  -->
         <div class="card">
             <div class="body">
                 <form>
@@ -39,7 +40,7 @@ require_once 'overview.php';
                                         <td><?php echo $row['description']; ?></td>
                                         <td><?php echo $row['status']; ?></td>
                                         <td>
-                                            <a href="/admin/edit-department.php?id=<?php echo $row['id']; ?>" class="btn btn-default btn-sm" title="Edit"><i class="fa fa-pencil"></i></a>
+                                            <a href="/admin/department-update.php?id=<?php echo $row['id']; ?>" class="btn btn-default btn-sm" title="Edit"><i class="fa fa-pencil"></i></a>
                                             <a href="/admin/delete-department.php?id=<?php echo $row['id']; ?>" class="btn btn-default btn-sm" title="Delete"><i class="fa fa-trash-o"></i></a>
                                         </td>
                                     </tr>
@@ -51,6 +52,7 @@ require_once 'overview.php';
                 </table>
             </div>
         </div>
+        <!-- Department stop here -->
     </div>
 </div>
 <?php
