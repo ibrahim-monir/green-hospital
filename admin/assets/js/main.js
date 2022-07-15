7e0ee72c80efd3d19f0b1514b48e3cfd670ff9eb
monzuralam <monzur707@gmail.com>
2022-07-15 08:15:39 +0600

    repeater.js init

--- admin/assets/js/main.js
+++ admin/assets/js/main.js
@@ -1,6 +1,46 @@
-$(document).ready(function(){ 
-    /* Create Repeater */
-    $("#repeater").createRepeater({
-        showFirstItemToDefault: true,
-    });
+$(document).ready(function () {
+    'use strict';
+    window.id = 0;
+
+    $('.repeater').repeater(
+        {
+            defaultValues: {
+                'id': window.id,
+
+            },
+            show: function () {
+                $(this).slideDown();
+                console.log($(this).find('input')[1]);
+                $('#cat-id').val(window.id);
+            },
+            hide: function (deleteElement) {
+                if (confirm('Are you sure you want to delete this element?')) {
+                    window.id--;
+                    $('#cat-id').val(window.id);
+                    $(this).slideUp(deleteElement);
+                    console.log($('.repeater').repeaterVal());
+                }
+            },
+            ready: function (setIndexes) {
+
+
+            }
+        }
+    );
+
 });
+
+// $(document).ready(function(){  
+//     $('#submit').click(function(){            
+//         $.ajax({  
+//             url:"confirm-prescription-add.php",  
+//             method:"POST",  
+//             data:$('#add_name').serialize(),  
+//             success:function(data)  
+//             {  
+//                 alert(data);  
+//                 $('#add_name')[0].reset();  
+//             }  
+//         });  
+//     });  
+// });  
