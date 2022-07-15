4473d08a8c5dea5d8e59c5b4ce0e813a2dc4f22e
monzuralam <monzur707@gmail.com>
2022-07-15 12:19:39 +0600

    footer added

---
+++ footer.php
@@ -0,0 +1,98 @@
+    <!-- footer area start -->
+    <footer class="site-footer site-footer-2 pt-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-3 col-lg-5">
+                    <div class="footer-widget">
+                        <h4 class="widget-title">About Us</h4>
+                        <p>Lorem ipsum dolor sit amet, consect etur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+                        <div class="social-links">
+                            <a href="#0"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#0"><i class="fab fa-twitter"></i></a>
+                            <a href="#0"><i class="fab fa-behance"></i></a>
+                            <a href="#0"><i class="fab fa-youtube"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-5 col-lg-7">
+                    <div class="opening-hour-box contact-support-info" data-overlay="100">
+                        <div class="opening-hour-top">
+                            <div class="icon">
+                                <img src="assets/images/icons/contact-icon.png" alt="">
+                            </div>
+                            <div class="content">
+                                <h5 class="title">Contact &amp; Support</h5>
+                                <p>It’s a fake timing. Actually we
+                                are available 24/7.</p>
+                            </div>
+                        </div>
+                        <div class="opening-hour-support-info">
+                            <div class="single-support-info">
+                                <h4 class="title">Phone</h4>
+                                <div class="content">
+                                    <a href="tel:01786860880">+880 1786 860880</a>
+                                </div>
+                            </div>
+                            <div class="single-support-info">
+                                <h4 class="title">Email</h4>
+                                <div class="content">
+                                    <a href="mailto:info@greenhms.com">info@greenhms.com</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-6 pl-0 pr-10">
+                    <div class="opening-hour-box opening-hour-box-3 bg_img ml-none-5" data-overlay="94" data-background="assets/images/bg/open-hour-bg.jpg">
+                        <div class="opening-hour-top">
+                            <div class="icon">
+                                <img src="assets/images/icons/clock-icon-red.png" alt="">
+                            </div>
+                            <div class="content">
+                                <h5 class="title">Opening Hours</h5>
+                                <p>It’s a fake timing. Actually we
+                                are available 24/7.</p>
+                            </div>
+                        </div>
+                        <div class="opening-hour-list">
+                            <ul>
+                                <li>Monday - Friday<span>8:00 - 16:00</span></li>
+                                <li>Saturday<span>8:00 - 12:00</span></li>
+                                <li>Sunday<span><strong>Closed</strong></span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="copyright-text copyright-text-2">
+                        <p>Copyright By @ <span>Green Hospital</span> - 2022</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- footer area end -->
+
+    <!--========= JS Here =========-->
+    <script src="assets/js/jquery-2.2.4.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCIJ_QKHN-bi6_1C9f5eYE3pZs1zhQIo5o"></script>
+    <script src="assets/js/counterup.min.js"></script>
+    <script src="assets/js/datepicker.min.js"></script>
+    <script src="assets/js/datepicker.en.js"></script>
+    <script src="assets/js/jquery-ui.min.js"></script>
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+    <script src="assets/js/lightcase.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/jquery.meanmenu.min.js"></script>
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/waypoint.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
