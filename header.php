750d82487ba825ce1fd2319bcc4498de0ac475a1
monzuralam <monzur707@gmail.com>
2022-07-15 12:19:29 +0600

    header added

---
+++ header.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+
+    <!--========= Required meta tags =========-->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--====== Title ======-->
+    <title>Green Hospital</title>
+
+    <!--====== Favicon ======-->
+    <link rel="shortcut icon" href="assets/images/logo/favicon.png" type="images/x-icon" />
+
+    <!--====== CSS Here ======-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/lightcase.css">
+    <link rel="stylesheet" href="assets/css/meanmenu.css">
+    <link rel="stylesheet" href="assets/css/nice-select.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/datepicker.min.css">
+    <link rel="stylesheet" href="assets/css/jquery-ui.min.css">
+    <link rel="stylesheet" href="assets/css/default.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+</head>
+
+<body>
+
+    <!-- header start -->
+    <header id="sticky-header" class="site-header">
+        <div class="container custom-header">
+            <div class="row align-items-center">
+                <div class="col-xl-3 col-lg-3">
+                    <a href="index.html" class="site-logo">
+                        <img src="assets/images/logo/logo.png" alt="">
+                    </a>
+                    <div class="mobile-menu"></div>
+                </div>
+                <div class="col-xl-6 col-lg-9 my-auto">
+                    <div class="mainmenu">
+                        <nav id="mobile-menu">
+                            <ul>
+                                <li>
+                                    <a href="/">Home</a>
+                                </li>
+                                <li>
+                                    <a href="departments.html">Department</a>
+                                </li>
+                                <li>
+                                    <a href="services.html">Doctors</a>
+                                </li>
+                                <li>
+                                    <a href="login.php">Appointment</a>
+                                </li>
+                                <li>
+                                    <a href="contact.html">Contact</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-xl-3 d-none d-xl-block">
+                    <div class="header-top-info d-flex align-items-center">
+                        <div class="icon">
+                            <img src="assets/images/icons/header-top-icon-1.png" alt="">
+                        </div>
+                        <div class="content">
+                            <p>Get Support</p>
+                            <a href="tel:01786860880">+880 (17) 868 608 80</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header end -->
