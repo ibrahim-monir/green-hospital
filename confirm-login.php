853b132a6a73d93de1764d0afed101259b859e0a
monzuralam <monzur707@gmail.com>
2022-07-18 00:25:30 +0600

    patient id query & store into session

--- confirm-login.php
+++ confirm-login.php
@@ -11,6 +11,18 @@ $query = mysqli_query($conn, $sql);
 if(mysqli_num_rows($query)){
     $_SESSION["login"] = 1;
     $_SESSION["patient"] = 1;
+
+    /**
+     * Patient ID Query
+     */
+    $patientSql = "SELECT * FROM patients WHERE mobile_number = {$mobile}";
+    $patientQuery = mysqli_query( $conn, $patientSql );
+    $patientData = mysqli_fetch_assoc( $patientQuery );
+    $_SESSION["patient_id"] = $patientData['id'];
+
+    /**
+     * Redirct to patient appointments add
+     */
     header( 'Location: /admin/appointments-patient-add.php' );
 }else{
     $_SESSION["error"] = "The mobile or password is invalid";
